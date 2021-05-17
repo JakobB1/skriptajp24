2dacd5341c5d1066afa1ac55d826aa472aceb49e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-05-17 10:34:47 +0100

    table smjer

--- skriptajp24.sql
+++ skriptajp24.sql
@@ -1,3 +1,11 @@
 drop database if exists skriptajp24;
 create database skriptajp24;
 use skriptajp24;
+
+create table smjer(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    trajanje int not null,
+    cijena decimal(18,2) null, # ako ne postoji not null onda se smatra da je null
+    certifikat boolean
+);
